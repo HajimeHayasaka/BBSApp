0442d2af5ed618b8b3baadcc6171b81eb4f83ea1
HajimeHayasaka <lunasa-inion-2.scannlain@i.softbank.jp>
2019-02-17 19:30:29 +0900

    - 新規作成。

---
+++ BBSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BBSApp
+//
+//  Created by 早坂甫 on 2019/02/17.
+//  Copyright © 2019年 早坂甫. All rights reserved.
+//z
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
